00e8811e681c11a399396d0550c0943eef4cd379
Melinda Cardenas <mc6377@nyu.edu>
2020-04-30 17:35:38 -0400

    Update db.js

---
+++ db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema({
+	username: String,
+	password: String,
+	followArr: Array
+
+})
+
+const Group = new Schema({
+	name: String,
+	membersArr: Array,
+	
+})
